bb96c8b4591f9c59e69c7a63a2d4c8480c8de3db
siba.mangal <63948501+Siba-mangal@users.noreply.github.com>
2023-04-17 20:58:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In thee middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("In thee middleware next");
+  // res.send("<h1>hello to node js </h1>");
+  res.send({ key1: value });
+});
+
+app.listen(3000);
