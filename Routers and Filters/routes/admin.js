e4d60212d2938c6bcf60b41c6b98482e78b7d783
siba.mangal <63948501+Siba-mangal@users.noreply.github.com>
2023-04-18 20:06:27 +0530

    Add files via upload

--- Routers and Filters/routes/admin.js
+++ Routers and Filters/routes/admin.js
@@ -1,1 +1,16 @@
+const express = require("express");
 
+const router = express.Router();
+
+router.get("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="number" name="productCount" min="1" max="100"><button type="submit">Add</button> </form>'
+  );
+});
+
+router.post("/add-product", (req, res, next) => {
+  console.log(req.body.title, req.body.productCount);
+  res.redirect("/admin/add-product");
+});
+
+module.exports = router;
