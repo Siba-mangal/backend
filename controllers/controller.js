cb83ec1d1c4a7381dbc7b0a37656914751191072
siba.mangal <63948501+Siba-mangal@users.noreply.github.com>
2023-04-26 10:21:46 +0530

    Add files via upload

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,1 +1,16 @@
+const path = require("path");
+const rootDir = require("../utill/path");
 
+exports.getContact = (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "contact.html"));
+};
+
+exports.postContact = (req, res, next) => {
+  res.send("<p>Form successfuly filled</p>");
+  res.redirect("/");
+};
+
+exports.successPost = (req, res, next) => {
+  res.send("<p>Form successfuly filled</p>");
+  res.redirect("/success");
+};
